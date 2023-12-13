54d60a1ba1a2545cfcce6583e25575e6cf340aa0
Quang Tu Atoma <quangtuagv@gmail.com>
2023-12-13 16:23:16 +0700

    Add test mpu6050, step motor, scanning i2c

---
+++ test/test_step_motor/test_step_motor.ino
@@ -0,0 +1,49 @@
+// the number of the LED pin
+const int ledPin = 14;  // 16 corresponds to GPIO16
+const int ledDir = 12;
+const int ledEn = 13;
+
+// setting PWM properties
+const int freq = 200;
+const int ledChannel = 0;
+const int resolution = 8;
+
+long delay_time = 2000;
+ 
+void setup(){
+  // configure LED PWM functionalitites
+  pinMode(ledEn, OUTPUT);
+  pinMode(ledDir, OUTPUT);
+  digitalWrite(ledEn, LOW);
+
+  ledcSetup(ledChannel, freq, resolution);
+  
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(ledPin, ledChannel);
+}
+ 
+void loop(){
+  // increase the LED brightness
+  
+  digitalWrite(ledDir, HIGH);
+  int dutyCycle = 5;
+  ledcWrite(ledChannel, dutyCycle);
+  
+  delay(delay_time);
+  digitalWrite(ledDir, LOW);
+  ledcWrite(ledChannel, dutyCycle);
+
+  delay(delay_time);
+  // for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+  //   // changing the LED brightness with PWM
+  //   ledcWrite(ledChannel, dutyCycle);
+  //   delay(15);
+  // }
+
+  // // decrease the LED brightness
+  // for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
+  //   // changing the LED brightness with PWM
+  //   ledcWrite(ledChannel, dutyCycle);   
+  //   delay(15);
+  // }
+}
